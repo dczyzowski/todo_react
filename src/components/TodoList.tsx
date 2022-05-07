d9d97b6946cabb61c2a52c7e4d2bc42408b86c90
Damian <dczyzowski@gmail.com>
2022-05-07 14:16:16 +0200

    hooks as useState

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,74 +1,88 @@
 import React, { useState, useEffect } from "react";
 
-export function TodoListView() {
-    interface Todo {
-        title: string;
-        description: string;
-    }
-    const initList : Todo[] = [
-        { title: "Zrobić todo liste", description: "zrobić edytowanie listy"},
-        { title: "Zrobić todo liste", description: "zrobić edytowanie listy"},
-        { title: "Zrobić todo liste", description: "zrobić edytowanie listy"}
-      ]
-      let title : string
-      const [list, updateList] = useState(initList)
+interface Todo {
+    title: string;
+    done: boolean;
+    finishDate?: Date;
+}
 
-    function addNewToList(newTodo: Todo){
+export function TodoListView() {
+
+    const [title, setTitle] = useState("")
+
+    const initList: Todo[] = [
+        { title: "Zrobić todo liste", done: true, finishDate: new Date("December 17, 1995 03:24:00") },
+        { title: "Zrobić sortowanie", done: false },
+        { title: "Zrobić edycje istniejących", done: false }
+    ]
+    const [list, updateList] = useState(initList)
+
+    function addNewToList(newTodo: Todo) {
         const newList: Todo[] = [];
         newList.push(...list, newTodo)
         updateList(newList)
-      }
+    }
 
-    function removeFromList(index: number){
+    function removeFromList(index: number) {
         const newList: Todo[] = [];
-        newList.push(...list)  
-        newList.splice(index,1)
+        newList.push(...list)
+        newList.splice(index, 1)
         updateList(newList)
     }
     return (
-    <div>
-        {/* <button onClick={sortByTitle}>Aa</button> */}
-        <ul>
-            {list.map((todo, index) => (
-                <li 
-                key={index}
-                >
-                    <TodoListItem title={todo.title} description={todo.description} />
-                    <button onClick={() => removeFromList(index)}>x</button>
-                </li>
-            ))
-            }
-        </ul>
-        <div id="addNewTodo">
-            <form >
-                <input 
-                onChange={
-                    e => title = e.target.value
-                } 
-                    type="text"/>
-                <button onClick={
-                    (e) => {
-                        e.preventDefault();
-                        addNewToList({title: title, description:"description"});
-                    }}>
-                         +
-                </button>
-            </form>
+        // TODO: add sort function
+        <div>
+            {/* <button onClick={sortByTitle}>Aa</button> */}
+            <ul>
+                {list.map((todo, index) => (
+                    <li
+                        key={index}
+                    >
+                        <TodoListItem
+                            title={todo.title}
+                            done={todo.done}
+                            finishDate={todo.finishDate}
+                        />
+                        <button onClick={() => removeFromList(index)}>x</button>
+                    </li>
+                ))
+                }
+            </ul>
+            <div
+                id="addNewTodo">
+                <form id="add-new-form">
+                    <input
+                        id="title-input"
+                        value={title}
+                        onChange={
+                            e => setTitle(e.target.value)
+                        }
+                        type="text" />
+                    <button onClick={
+                        (e) => {
+                            e.preventDefault();
+                            addNewToList({ title: title, done: false, finishDate: new Date("December 17, 1995 03:24:00") });
+                            setTitle("")
+                        }}>
+                        +
+                    </button>
+                </form>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
-function TodoListItem(props: {title: string, description: string}) {
+function TodoListItem(props: Todo) {
     const [done, changeState] = useState(false)
     const [deleted, deleteTodo] = useState(false)
     return (
-        <div id="item" style={{textDecoration: done ? 'line-through' : undefined }}>
+        <div id="item" style={{ textDecoration: done ? 'line-through' : undefined }}>
             <label>
-                <p id="title" onClick={() => {changeState(!done); console.log('klik')}}>{props.title}</p>
-                <p id="description">{props.description}</p>
+                <p id="title" onClick={() => { changeState(!done); console.log('klik') }}>{props.title}</p>
+                {props.finishDate != undefined ?
+                    <p id="date">{props.finishDate.toDateString()}</p> : <></>}
+
             </label>
         </div>
     );
 };
-
