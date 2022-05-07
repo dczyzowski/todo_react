8c19a794818ca53e1dcd0d30e9af8d279277d427
Damian <dczyzowski@gmail.com>
2022-05-07 14:11:21 +0200

    added MUI Components

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,74 +1,134 @@
 import React, { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ListItemText from '@mui/material/ListItemText';
+
+
+interface Todo {
+    title: string;
+    done: boolean;
+    finishDate?: Date;
+}
 
 export function TodoListView() {
-    interface Todo {
-        title: string;
-        description: string;
-    }
-    const initList : Todo[] = [
-        { title: "Zrobić todo liste", description: "zrobić edytowanie listy"},
-        { title: "Zrobić todo liste", description: "zrobić edytowanie listy"},
-        { title: "Zrobić todo liste", description: "zrobić edytowanie listy"}
-      ]
-      let title : string
-      const [list, updateList] = useState(initList)
 
-    function addNewToList(newTodo: Todo){
+    const [title, setTitle] = useState("")
+
+    const initList: Todo[] = [
+        { title: "Zrobić todo liste", done: true, finishDate: new Date("December 17, 1995 03:24:00") },
+        { title: "Zrobić sortowanie", done: false },
+        { title: "Zrobić edycje istniejących", done: false }
+    ]
+    const [list, updateList] = useState(initList)
+
+    function addNewToList(newTodo: Todo) {
         const newList: Todo[] = [];
         newList.push(...list, newTodo)
         updateList(newList)
-      }
+    }
 
-    function removeFromList(index: number){
+    function removeFromList(index: number) {
         const newList: Todo[] = [];
-        newList.push(...list)  
-        newList.splice(index,1)
+        newList.push(...list)
+        newList.splice(index, 1)
         updateList(newList)
     }
+
+    function handleSubmit(e: React.FormEvent) {
+        //TODO: add date picker
+        e.preventDefault();
+        addNewToList({ title: title, done: false, finishDate: new Date("December 17, 1995 03:24:00") });
+        setTitle("")
+    }
+
     return (
-    <div>
-        {/* <button onClick={sortByTitle}>Aa</button> */}
-        <ul>
-            {list.map((todo, index) => (
-                <li 
-                key={index}
-                >
-                    <TodoListItem title={todo.title} description={todo.description} />
-                    <button onClick={() => removeFromList(index)}>x</button>
-                </li>
-            ))
-            }
-        </ul>
-        <div id="addNewTodo">
-            <form >
-                <input 
-                onChange={
-                    e => title = e.target.value
-                } 
-                    type="text"/>
-                <button onClick={
-                    (e) => {
-                        e.preventDefault();
-                        addNewToList({title: title, description:"description"});
-                    }}>
-                         +
-                </button>
-            </form>
+        // TODO: add sort function
+        <div style={{maxWidth: "500px", margin: "16px", padding:"16px", boxShadow: "4px 4px 16px -16px rgba(66, 68, 90, 1)"}}>
+            {/* <button onClick={sortByTitle}>Aa</button> */}
+            <List>
+                {list.map((todo, index) => (
+                    <ListItem
+                        key={index}
+                        secondaryAction={
+                            <IconButton onClick={() => removeFromList(index)}><DeleteIcon/></IconButton>
+                        }
+                    >
+                        <TodoListItem
+                            title={todo.title}
+                            done={todo.done}
+                            finishDate={todo.finishDate}
+                        />
+                    </ListItem>
+                ))
+                }
+            </List>
+            <div
+                id="addNewTodo">
+                <form id="add-new-form" onSubmit={(e) => handleSubmit(e)}>
+                    <TextField
+                        id="outlined-basic"
+                        label="Outlined"
+                        variant="outlined"
+                        value={title}
+                        autoComplete="off"
+                        onChange={
+                            e => setTitle(e.target.value)
+                        }
+                        type="text" />
+                    <Button
+                        variant="outlined"
+                        onClick={
+                            (e) => { handleSubmit(e) }}>
+                        +
+                    </Button>
+                </form>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
-function TodoListItem(props: {title: string, description: string}) {
+function TodoListItem(props: Todo) {
     const [done, changeState] = useState(false)
     const [deleted, deleteTodo] = useState(false)
     return (
-        <div id="item" style={{textDecoration: done ? 'line-through' : undefined }}>
-            <label>
-                <p id="title" onClick={() => {changeState(!done); console.log('klik')}}>{props.title}</p>
-                <p id="description">{props.description}</p>
-            </label>
+        <div id="item" style={{ textDecoration: done ? 'line-through' : undefined }}>
+            <ListItemText
+                    primary={props.title}
+                    onClick={() => { changeState(!done); console.log('klik') }}
+                    secondary={props.finishDate ? props.finishDate.toDateString() : null}
+                  />
         </div>
     );
 };
 
+// {this.state.weatherData != null
+//     ? <Weather weatherData={this.state.weatherData}/>
+//     : <p>Loading...</p>
+// }
+
+
+// <List dense={dense}>
+// {generate(
+//   <ListItem
+//     secondaryAction={
+//       <IconButton edge="end" aria-label="delete">
+//         <DeleteIcon />
+//       </IconButton>
+//     }
+//   >
+//     <ListItemAvatar>
+//       <Avatar>
+//         <FolderIcon />
+//       </Avatar>
+//     </ListItemAvatar>
+//     <ListItemText
+//       primary="Single-line item"
+//       secondary={secondary ? 'Secondary text' : null}
+//     />
+//   </ListItem>,
+// )}
+// </List>
